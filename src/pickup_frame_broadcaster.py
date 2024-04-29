85d4aeb062d03d7c5c658de243460094efb08c27
James <leejs8128@gmail.com>
2024-04-29 15:50:24 -0400

    Add the camera-calibration-error offsets to the camera frame broadcaster.

--- src/pickup_frame_broadcaster.py
+++ src/pickup_frame_broadcaster.py
@@ -12,20 +12,20 @@ class PickupFrameBroadcaster:
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         
         new_tfs = TransformStamped()
-        new_tfs.header.frame_id = 'cargo'
+        new_tfs.header.frame_id = 'fiducial_1'
         new_tfs.child_frame_id = 'pickup'
 
         rate = rospy.Rate(10.0)
 
         while not rospy.is_shutdown():
             try:
-                tf_cargo = self.tf_buffer.lookup_transform('cargo', 'cargo', rospy.Time()).transform
+                tf_cargo = self.tf_buffer.lookup_transform('fiducial_1', 'fiducial_1', rospy.Time()).transform
                 new_tfs.header.stamp = rospy.Time.now()
                 new_tfs.transform.translation = tf_cargo.translation
                 new_tfs.transform.translation.x = tf_cargo.translation.x
                 new_tfs.transform.translation.y = tf_cargo.translation.y
 
-                tf_cargo_to_base_link = self.tf_buffer.lookup_transform('cargo', 'px100/base_link', rospy.Time()).transform 
+                tf_cargo_to_base_link = self.tf_buffer.lookup_transform('fiducial_1', 'px100/base_link', rospy.Time()).transform 
                 new_tfs.transform.rotation = tf_cargo_to_base_link.rotation
 
                 self.tf_broadcaster.sendTransform(new_tfs)
